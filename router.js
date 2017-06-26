d888fad316d0958ec4a25cb127161f5fb87cc864
KonradM <miekus.konrad@gmail.com>
2017-06-26 22:44:23 +0200

    Refactor syntax

--- router.js
+++ router.js
@@ -7,10 +7,8 @@ const requireAuth = passport.authenticate('jwt', { session : false })
 const requireSignin = passport.authenticate('local', { session : false })
 
 module.exports = (app) => {
-  app.get('/', requireAuth, (req, res) => {
-  	res.send({hi: 'there'})
-  })
   
+  app.get('/', requireAuth, (req, res) => res.send({hi: 'there'}))
   app.post('/signup', Authentication.signup)
   app.post('/signin', requireSignin, Authentication.signin)
   app.get('/health', (req, res) => res.send('I\'m aliveeee !'))
